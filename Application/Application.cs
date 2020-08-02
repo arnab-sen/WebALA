5497d77fe815313c4392536dcd7deeb5617e9744
Arnab Sen <arnab.sen210@gmail.com>
2020-08-02 17:53:05 +1200

    Initial commit

--- Application/Application.cs
+++ Application/Application.cs
@@ -1,12 +1,25 @@
-﻿using System;
+using System;
+using Libraries;
+using ProgrammingParadigms;
+using DomainAbstractions;
 
 namespace WebALA
 {
     class Application
     {
+        private EventConnector appStart = new EventConnector() { InstanceName = "appStart" };
+
+        // BEGIN AUTO-GENERATED INSTANTIATIONS FOR Application.xmind
+        // END AUTO-GENERATED INSTANTIATIONS FOR Application.xmind
+
+        // BEGIN AUTO-GENERATED WIRING FOR Application.xmind
+        // END AUTO-GENERATED WIRING FOR Application.xmind
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var app = new Application();
+            Wiring.PostWiringInitialize();
+            (app.appStart as IEvent).Execute();
         }
     }
 }
