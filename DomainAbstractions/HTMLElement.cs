5af9b2fc810fcfd61593f8d8e69f48c390a6d7dc
Arnab Sen <arnab.sen210@gmail.com>
2020-08-02 18:43:22 +1200

    New domain abstraction: HTMLElement

---
+++ DomainAbstractions/HTMLElement.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Libraries;
+using ProgrammingParadigms;
+
+namespace DomainAbstractions
+{
+    public class HTMLElement : INestedText
+    {
+        // Public fields and properties
+        public string InstanceName = "Default";
+        public string Type { get; set; }
+        public string Content { get; set; }
+
+        // Private fields
+        private string _htmlCode;
+
+        // Ports
+        private List<INestedText> childContent = new List<INestedText>();
+
+        public HTMLElement()
+        {
+
+        }
+
+        // INestedText implementation
+        string INestedText.GetText()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"<{Type}>");
+
+            sb.AppendLine(Content);
+
+            foreach (var nestedText in childContent)
+            {
+                sb.AppendLine(nestedText.GetText());
+            }
+
+            sb.AppendLine($"/<{Type}>");
+
+            _htmlCode = sb.ToString();
+
+            return _htmlCode;
+        }
+    }
+}
